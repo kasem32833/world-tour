0620f77e4d0f9f3a423ca25b108472684d47d743
Kasem32833 <abulkasem32833@gmail.com>
2023-09-11 21:15:58 +0300

    load flags and country name from api

--- src/Componnts/Countries/Countries.jsx
+++ src/Componnts/Countries/Countries.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import Country from "../Country/Country";
+import '../Countries/countries.css'
 
 export default function Countries (){
     const [countries, setCountries] = useState([])
+    console.log(countries);
 
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
@@ -12,10 +14,10 @@ export default function Countries (){
     return (
         
 
-        <div>
+        <div className="box">
             <h3>Countries:{countries.length}</h3>
             {
-                countries.map(country => <Country countries = {countries}></Country>)
+                countries.map(country => <Country key={countries.ccad} country = {country}></Country>)
             }
         </div>
        
--- src/Componnts/Country/Country.jsx
+++ src/Componnts/Country/Country.jsx
@@ -1,11 +1,12 @@
+import '../Country/country.css'
 
-const Country = (countries) => {
-    console.log(countries)
-    
-
+const Country = ({country}) => {
+    const {name, flags} = country;
+console.log(flags.png)
     return (
-        <div>
-            
+        <div className="box-style">
+            <h4>Country:{name?.common}</h4>
+            <img width={'200px'} height={'150px'} src= {flags.png} alt="" />
         </div>
     );
 };
